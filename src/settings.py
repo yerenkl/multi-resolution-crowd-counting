1764ef135310b287180fc826e8b3759f93309e09
linusjuni <linus@juni.dk>
2026-04-23 15:22:19 +0200

    bugfix

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file_encoding="utf-8", case_sensitive=True)¨
+    model_config = SettingsConfigDict(env_file_encoding="utf-8", case_sensitive=True)
 
     RANDOM_SEED: int = 42
 
